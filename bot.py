e2424d9f7f519bfefe1e6e97ae471dea06f17240
brandon68 <67684399+brandon68@users.noreply.github.com>
2026-09-19 21:44:29 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ CATALOGO_PUBLICIDAD = [
         )
     },
     {
-        "hora": (19, 5),  # 21:43 PM
+        "hora": (21, 46),  # 21:43 PM
         "imagen": "ANUNCIOTAREJTAS.jpg",
         "texto": (
             "⚡ <b>¡CHECKER CCS TELEGRAM!</b> ⚡\n\n"
